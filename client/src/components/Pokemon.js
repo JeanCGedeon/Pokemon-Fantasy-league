27fc99a488e81752220d8d15a53dd80dfd82ca33
Jean Gedeon <carlenssg@gmail.com>
2022-12-17 14:41:00 -0500

    initial start

---
+++ client/src/components/Pokemon.js
@@ -0,0 +1,11 @@
+import React from "react"
+const Pokemon = (props) => {
+
+
+
+    return(
+        <h1>hi</h1>
+    )
+}
+
+export default Pokemon
---
+++ server/src/routes/api/v1/pokemonRouter.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+
+
+const pokemonRouter = new express.Router();
+
+
+
+export default pokemonRouter;
